44014a4d1028549b7d2cef59404003eea08ff15e
Peter Chen <ch.focke@gmail.com>
2024-02-29 17:59:51 +0800

    feat: add simpleTrans

--- trans/trans.go
+++ trans/trans.go
@@ -4,3 +4,18 @@ type Trans interface {
 	Send([]byte) error
 	Close()
 }
+
+func NewSimpleTrans(myFunc func([]byte) error) Trans {
+	return &simpleTrans{sendFunc: myFunc}
+}
+
+type simpleTrans struct {
+	sendFunc func([]byte) error
+}
+
+func (t *simpleTrans) Send(data []byte) error {
+	return t.sendFunc(data)
+}
+
+func (t *simpleTrans) Close() {
+}
